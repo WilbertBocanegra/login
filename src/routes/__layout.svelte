55c773d8962dfca39617f22d3e20faf181aa43df
Wilbert Bocanegra <wilbertbocanegra@gmail.com>
2022-07-05 12:02:30 -0500

    tw commit

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,21 +1,6 @@
 <script>
-	import '@softengy/components/src/app.css';
 	import '../app.css';
-	import { setClient } from '@softengy/components';
-	/**
-	 * @type {RequestInfo | URL}
-	 * @const
-	 */
-	const endpoint = 'http://localhost:3000';
-	setClient({
-		endpoint: endpoint,
-		config: {
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			credentials: 'include'
-		}
-	});
+
 </script>
 
 <slot />
